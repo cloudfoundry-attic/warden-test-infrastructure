5b76f7867830eaf24f1cb053731a46d7d97fe339
David Stevenson and Matt Royal <pair+stevend+mroyal@pivotallabs.com>
2013-04-04 13:42:49 -0700

    Split box building into a new script

--- create_vagrant_and_run_test.sh
+++ create_vagrant_and_run_test.sh
@@ -12,48 +12,9 @@ BUILD_TO_RUN_PATH=$1
 TEST_INFRA_PATH=$2
 TMP_FOLDER_PATH=`pwd`
 
-mkdir -p ~/boxes
 if [[ ! -f ~/boxes/ci_with_warden_prereqs.box ]]; then
-  if [[ ! -d travis-cookbooks ]]; then
-    git clone https://github.com/travis-ci/travis-cookbooks.git
-  fi
-  (
-    cd travis-cookbooks
-    git fetch https://github.com/travis-ci/travis-cookbooks.git
-    git checkout 77605d7405dd97e1b418965d3d8fa481030d6117
-  )
-
-  cp -r $TEST_INFRA_PATH/ci-cookbooks .
-  cat <<EOF > Vagrantfile
-    Vagrant.configure("2") do |config|
-      config.ssh.username = "travis"
-      config.vm.define "$VM_NAME" # give the VM a unique name
-      config.vm.box = "travis-base"
-      config.vm.box_url = "http://files.travis-ci.org/boxes/bases/precise64_base_v2.box"
-      config.vm.provision :chef_solo do |chef|
-        chef.cookbooks_path = ['travis-cookbooks/ci_environment', 'ci-cookbooks']
-        chef.add_recipe 'git'
-        chef.add_recipe 'unzip'
-        chef.add_recipe 'rvm::multi'
-        chef.add_recipe 'sqlite'
-        chef.add_recipe 'mysql::server'
-        chef.add_recipe 'postgresql::server'
-        chef.add_recipe 'redis'
-        chef.add_recipe 'warden'
-        chef.json = {
-          "rvm" => {
-            "default" => "1.9.3",
-            "rubies" => [{"name" => "1.9.3"}]
-          }
-        }
-      end
-    end
-EOF
-  cat Vagrantfile
-  vagrant up
-  vagrant package $VM_NAME --output ~/boxes/ci_with_warden_prereqs.box
-  vagrant box remove ci_with_warden_prereqs virtualbox  # this will fail the first time, that's okay
-  vagrant destroy --force
+  echo "NO vagrant box found in ~/boxes, you probably need to run create_vagrant_box.sh first!"
+  exit 1
 fi
 
 cat <<EOF >Vagrantfile
