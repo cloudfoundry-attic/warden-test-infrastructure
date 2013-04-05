470e083a65d77f99f69d42666c708b3b34b42457
Matthew Boedicker & Max Brunsfeld <pair+matthew+max@pivotallabs.com>
2013-04-05 11:12:36 -0700

    Make warden and rootfs part of base VM

--- start_warden.sh
+++ start_warden.sh
@@ -1,30 +1,8 @@
 #!/bin/bash
 set -e -u -x
 (
-  git clone --depth=1 --quiet --branch=master git://github.com/cloudfoundry/warden.git vendor/warden
-  cd vendor/warden
-
-  # Ignore this project's BUNDLE_GEMFILE
-  unset BUNDLE_GEMFILE
-
-  # Close stdin
-  exec 0>&-
-
-  # Remove remnants of apparmor (specific to Travis VM)
-  sudo dpkg --purge apparmor
-
-  # Install dependencies
-  # sudo apt-get -y install debootstrap quota
-
-  cd warden
-  rvmsudo bundle install
-
-  # Download and extract the warden rootfs
-  rvmsudo bundle exec rake setup:bin[config/linux.yml]
-  mkdir -p /tmp/warden/rootfs
-  curl -s http://d31qcsjlqa9q7y.cloudfront.net/lucid64.latest.tgz | sudo tar xzf - -C /tmp/warden/rootfs
-
-  rvmsudo bundle exec rake warden:start[config/linux.yml] --trace >/dev/null 2>&1 &
+  cd /warden/warden
+  rvmsudo bundle exec rake warden:start[config/test_vm.yml] --trace >/dev/null 2>&1 &
 )
 
 # Wait for warden to come up
