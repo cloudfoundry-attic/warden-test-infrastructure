6890c70a1cfc8a38382932c7d389717229df4b33
Ruth Byers <rbyers@pivotallabs.com>
2013-12-12 11:46:05 -0800

    Add timestamps to test script logging.
    Signed-off-by: Eric Malm <emalm@pivotallabs.com>

--- create_vagrant_and_run_test.sh
+++ create_vagrant_and_run_test.sh
@@ -8,6 +8,9 @@ VM_NAME=default
 WARDENIZED_SERVICE=${WARDENIZED_SERVICE:-}
 REQUIRE_PACKAGE=${REQUIRE_PACKAGE:-}
 
+echo "at the beginning"
+echo date
+
 # best effort to command in critical section
 function lock {
   LOCKFILE=/run/shm/vagrantup.lock
@@ -33,15 +36,27 @@ cat <<EOF >Vagrantfile
   end
 EOF
 
+echo "before vagrant up:"
+echo date
+
 lock vagrant up
+
+echo "after vagrant up:"
+echo date
 
 vagrant ssh-config > ssh_config
 rsync -arq --rsh="ssh -F ssh_config" $BUILD_TO_RUN_PATH/ $VM_NAME:workspace
 rsync -arq --rsh="ssh -F ssh_config" $TEST_INFRA_PATH/start_warden.sh $VM_NAME:workspace/
 
+echo "after rsync stuff"
+echo date
+
 echo "Your vagrant box is now provisioned in folder $TMP_FOLDER_PATH! Don't forget to vagrant destroy it eventually."
 echo "To connect: vagrant ssh $VM_NAME"
 echo "To destroy: vagrant destroy $VM_NAME"
+
+echo "about to ssh to run tests"
+echo date
 
 if [ -z ${NOTEST:=} ]; then
   vagrant ssh $VM_NAME -c "cd ~/workspace &&     \
