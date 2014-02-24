6d850f90d2fd56122aab1af85b75c08a411cdceb
Dr Nic Williams <drnicwilliams@gmail.com>
2014-02-24 10:00:43 -0800

    Ensure travis_build_environment.group is also vagrant

--- create_vagrant_box.sh
+++ create_vagrant_box.sh
@@ -39,6 +39,7 @@ cat <<EOF > Vagrantfile
         },
         "travis_build_environment" => {
           "user" => "vagrant",
+          "group" => "vagrant",
           "home" => "/home/vagrant"
         }
       }
