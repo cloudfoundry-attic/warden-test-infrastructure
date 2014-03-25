147068b03470c681e0c346eb1481d8ca839456ea
Abhijit Hiremagalur <abhi@pivotallabs.com>
2014-03-25 16:17:03 -0700

    Remove unused files
    Signed-off-by: Michael Fraenkel <fraenkel@us.ibm.com>

--- packaging/package_mysql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -x -u
-
-SERVICE_NAME="mysql"
-TARGET_HOST=10.10.4.31  # ip of mysql node on tabasco
-TAR_UP_STORE="mysql_common"
-TAR_UP_PACKAGES="{mysql55,mysqlclient}"
-source ./package.sh
--- packaging/package_rabbit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e -x -u
-
-SERVICE_NAME="rabbitmq"
-TARGET_HOST=10.10.4.72  # ip of mysql node on tabasco
-TAR_UP_STORE="rabbitmq_common"
-TAR_UP_PACKAGES="{erlang,rabbitmq-*}"
-source ./package.sh
