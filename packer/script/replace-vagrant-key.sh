8d1da4bfe3bc7d6dd2044d46d65a9a5edce1a42b
Matthew Sykes <matthew.sykes@gmail.com>
2014-05-30 13:04:55 -0700

    Remove well-known vagrant keys from aws image
    Generated a private key pair for vagrant to be used for CI.
    
    Signed-off-by: Chris Adams <cadams@pivotallabs.com>

---
+++ packer/script/replace-vagrant-key.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -eux
+
+echo ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC/xnFbY6TjmMEYVekv+qtgTQxtBnnAfWD7sKWFHQ0Mj/aNL5/O+Dv3WyYo/8n4ZR8ku/OEyG1AefuGT9+FHqK+RwrWdOmocbtGZMGS7VPNp7WzjHFNOdn8f/xZNj/rLG3xLL/HRUtG9ATs7lJmQobMfKveU7goRdYnY37/E0hlRci7gliXhU4dEwUWlmh76guHgAs8ShYnsp1cZLrLAxjavPLr1oIzjP73JxaCojIa3Alh3Sl0+PiwqTWsLn2xkteFcDS+yhYt3pUFANy/REFvABpNSXB+T8GBB7azqwJ2odUdh73XbijsFQfZr2U5EI5wnHzUsHpRLPPpJMfsYJoV id_rsa_warden-test-infrastructure > ~vagrant/.ssh/authorized_keys
+
