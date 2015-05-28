674aec9002a89d547688c70ef6b855ebc82a5b60
Joseph Palermo and Marc Schunk <jpalermo@pivotal.io>
2015-05-28 14:41:19 -0700

    Remove lucid stack
    [#95483678]

--- site-cookbooks/warden/recipes/default.rb
+++ site-cookbooks/warden/recipes/default.rb
@@ -1,9 +1,7 @@
 WARDEN_PATH = "/warden"
-ROOT_FS_PATH_LUCID = "/var/warden/rootfs_lucid"
 ROOT_FS_PATH_CFLINUXFS2 = "/var/warden/rootfs_cflinuxfs2"
-ROOT_FS_URL_LUCID =  "http://cf-runtime-stacks.s3.amazonaws.com/lucid64.dev.tgz"
 ROOT_FS_URL_CFLINUXFS2 = "http://cf-runtime-stacks.s3.amazonaws.com/cflinuxfs2.dev.tgz"
-DEFAULT_ROOT_FS_PATH = ROOT_FS_PATH_LUCID
+DEFAULT_ROOT_FS_PATH = ROOT_FS_PATH_CFLINUXFS2
 OLD_CONFIG_FILE_PATH = "#{WARDEN_PATH}/warden/config/linux.yml"
 NEW_CONFIG_FILE_PATH = "#{WARDEN_PATH}/warden/config/test_vm.yml"
 
@@ -50,18 +48,6 @@ execute "download warden cflinuxfs2 rootfs from s3" do
     mkdir -p #{ROOT_FS_PATH_CFLINUXFS2}
     curl -s #{ROOT_FS_URL_CFLINUXFS2} | tar xzf - -C #{ROOT_FS_PATH_CFLINUXFS2}
   BASH
-end
-
-execute "download warden lucid rootfs from s3" do
-  command <<-BASH
-    rm -rf #{ROOT_FS_PATH_LUCID}
-    mkdir -p #{ROOT_FS_PATH_LUCID}
-    curl -s #{ROOT_FS_URL_LUCID} | tar xzf - -C #{ROOT_FS_PATH_LUCID}
-  BASH
-end
-
-execute "copy resolv.conf from lucid outside container" do
-  command "cp /etc/resolv.conf #{ROOT_FS_PATH_LUCID}/etc/resolv.conf"
 end
 
 execute "copy resolv.conf from cflinuxfs2 outside container" do
