6904e67c89e0e2f528dee6352adb835988d1f3de
Michael Fraenkel <fraenkel@us.ibm.com>
2016-01-19 08:52:44 -0500

    Switch to Go 1.5, Update README
    Signed-off-by: Sandy Cash <scarlet.tanager@gmail.com>

--- packer/script/golang.sh
+++ packer/script/golang.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
 
-# install Go 1.4
-wget -qO- https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+# install Go 1.5
+wget -qO- https://storage.googleapis.com/golang/go1.5.3.linux-amd64.tar.gz | tar -C /usr/local -xzf -
 echo "export PATH=/usr/local/go/bin:$PATH" > /etc/profile.d/golang.sh
