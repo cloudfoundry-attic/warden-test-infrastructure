fa5bab9f6f85ecd4f0a27491756e974e65e0ee59
Sandy Cash <scarlet.tanager@gmail.com>
2016-04-29 18:06:24 -0700

    Bump ruby in warden-compatible box
    [#118638259]
    
    Signed-off-by: Swetha Repakula <srepaku@us.ibm.com>

--- packer/script/ruby-install.sh
+++ packer/script/ruby-install.sh
@@ -4,7 +4,7 @@
 git clone https://github.com/rbenv/ruby-build.git /tmp/ruby-build && \
 cd /tmp/ruby-build && \
 PREFIX=/usr/local ./install.sh && \
-/usr/local/bin/ruby-build 2.2.4 /usr && \
+/usr/local/bin/ruby-build 2.3.0 /usr && \
 cd && \
 rm -rf /tmp/ruby-build && \
 gem install bundler --no-ri --no-doc
