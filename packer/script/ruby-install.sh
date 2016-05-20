f0d173506bf39742b6ddad00ccc3846824ec5f09
Dan Lavine <dlavine@us.ibm.com>
2016-05-20 14:21:09 -0700

    update bundler version to 1.12.4
    [#119093469]

--- packer/script/ruby-install.sh
+++ packer/script/ruby-install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
 
-# install Ruby 2.2.4
+# install Ruby 2.3.1
 git clone https://github.com/rbenv/ruby-build.git /tmp/ruby-build && \
 cd /tmp/ruby-build && \
 PREFIX=/usr/local ./install.sh && \
 /usr/local/bin/ruby-build 2.3.1 /usr && \
 cd && \
 rm -rf /tmp/ruby-build && \
-gem install bundler -v '1.11.2' --no-ri --no-doc
+gem install bundler -v '1.12.4' --no-ri --no-doc
